b00053aaefe4926ad700195d009230171ddd1a42
cub@libre <97299329+cub4libre@users.noreply.github.com>
2022-03-25 20:10:40 +0800

    Update base64sql.py

--- base64sql.py
+++ base64sql.py
@@ -22,7 +22,7 @@ DBdata = [[]]
 flag = "那你就是诗人"
 # 请求头的参数
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
-          "Origin": "http://150.158.27.164:2081",
+          "Origin": "xxxxxxx",
           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
           "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
           "Content-Type": "application/x-www-form-urlencoded", "Upgrade-Insecure-Requests": "1"}
